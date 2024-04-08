bbc520abc25a49b32ef22214f8366a0d75b2171e
nicolas.binatti <nicolas.binatti@scccheck.com.br>
2024-04-08 05:51:30 -0300

    filtering by url

--- src/app/report-list-all-movies/report-list-all-movies.component.ts
+++ src/app/report-list-all-movies/report-list-all-movies.component.ts
@@ -1,8 +1,7 @@
-import {Component, ViewChild, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatTableModule} from '@angular/material/table';
-import {MatTableDataSource} from '@angular/material/table';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {FormsModule} from "@angular/forms";
 import {MatOption, MatSelect} from "@angular/material/select";
 import {MatGridList, MatGridTile} from "@angular/material/grid-list";
@@ -10,6 +9,7 @@ import {MatPaginator} from '@angular/material/paginator';
 import {Movies} from "../interfaces/movies.interface";
 import {MoviesService} from "../services/movies.service";
 import {NgIf} from "@angular/common";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-report-list-all-movies',
@@ -25,6 +25,7 @@ import {NgIf} from "@angular/common";
     MatGridTile,
     MatPaginator,
     NgIf,
+    MatButton,
   ],
   templateUrl: './report-list-all-movies.component.html',
   styleUrl: './report-list-all-movies.component.scss'
@@ -41,51 +42,36 @@ export class ReportListAllMoviesComponent implements OnInit {
   };
 
   table = {
-    data : new MatTableDataSource<Movies>([]),
+    data: new MatTableDataSource<Movies>([]),
     headers: ['id', 'year', 'title', 'winner']
-
   }
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator | null = null;
 
-  async ngOnInit() {
-    await this.moviesService.getMoviesList()
-    this.table.data.data = this.moviesService.movieList
-
-    if (this.paginator) {
-      this.table.data.paginator = this.paginator;
-    }
+  ngOnInit() {
+    this.applyFilter()
   }
 
+  async applyFilter() {
+    try {
+      this.table.data.data = await this.moviesService.getMoviesList(
+        this.filter.title,
+        this.filter.year,
+        this.filter.winner
+      );
 
-
-  applyFilter() {
-    this.table.data.filterPredicate = (data, filter) => {
-      const filterObject = JSON.parse(filter);
-      let yearMatch = true;
-      let titleMatch = true;
-      let winnerMatch = true;
-
-      if (filterObject.year) {
-        yearMatch = data.year.toString().includes(filterObject.year);
+      if (this.paginator) {
+        this.table.data.paginator = this.paginator;
       }
+      this.table.data.paginator = this.paginator;
 
-      if (filterObject.title) {
-        titleMatch = data.title.toLowerCase().includes(filterObject.title.toLowerCase());
+      if (this.table.data.paginator) {
+        this.table.data.paginator.firstPage();
       }
-
-      if (filterObject.winner !== null && filterObject.winner !== undefined) {
-        winnerMatch = data.winner === filterObject.winner;
-      }
-
-      return yearMatch && titleMatch && winnerMatch;
-    };
-
-    this.table.data.filter = JSON.stringify(this.filter);
-
-    if (this.table.data.paginator) {
-      this.table.data.paginator.firstPage();
+    } catch (error) {
+      console.error('Fail to get movies list:', error);
     }
+
   }
 }
 
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,23 +1,30 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {environment} from "../../environment";
-import {Movies} from "../interfaces/movies.interface";
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
 
-  movieList: Movies[];
-
   constructor(private http: HttpClient) {
-    this.movieList = [];
   }
 
-  async getMoviesList() {
-    return this.http.get<any>(environment.apiUrl + 'movies')
-      .subscribe(data => {
-        this.movieList = data
-      })
+  async getMoviesList(title: string | null, year?: number | null, winner?: boolean | null): Promise<any> {
+    let endpoint: string = environment.apiUrl + 'movies';
+    let params = new HttpParams();
+
+    if (title) {
+      params = params.append('winner', title.toString());
+    }
+    if (year) {
+      params = params.append('year', year.toString());
+    }
+    if (winner) {
+      params = params.append('winner', winner.toString());
+    }
+
+    return this.http.get<any>(endpoint, {params}).toPromise();
+
   }
 }
